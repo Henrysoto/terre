57061f17da984dd07fd5d0cb5b04296873d6ad17
Henrysoto <rtcwpub@gmail.com>
2023-06-13 11:38:36 +0200

    terre10 OK

--- terre10.go
+++ terre10.go
@@ -5,12 +5,17 @@ import (
 	"math"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	if len(os.Args) == 2 {
 		if int(os.Args[1][0]) == int('-') {
-			fmt.Println("exposant négatif")
+			fmt.Println("nombre positif uniquement")
+			return
+		}
+		if strings.ContainsAny(os.Args[1], ".,") {
+			fmt.Println("nombre entier uniquement")
 			return
 		}
 		a, _ := strconv.ParseFloat(os.Args[1], 10)
